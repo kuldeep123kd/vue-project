e0760b34bc2e1c02d4373fedc0db0e3350997c43
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-03-05 19:03:41 +0530

    element ui plus installed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+import locale from "element-plus/lib/locale/lang/en";
+
 // import FriendContact from "./components/FriendContact.vue";
 // import NewFriend from "./components/NewFriend.vue";
 
-// const app = createApp(App);
-createApp(App).mount("#app");
+const app = createApp(App);
+// createApp(App).mount("#app");
+
+app.use(ElementPlus, { locale });
 
 // app.component("friend-contact", FriendContact);
 // app.component("new-friend", NewFriend);
 
-// app.mount("#app");
+app.mount("#app");
